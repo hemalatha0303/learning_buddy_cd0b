a68f177632841aa2a0077b3a80ffe8cdc1a099fc
Hemalatha Donapati <hemalathareddy.7254@gmail.com>
2025-07-10 20:08:43 +0530

    Update get_connection.py

--- get_connection.py
+++ get_connection.py
@@ -1,11 +1,19 @@
-
-import os
+# get_connection.py
 import psycopg2
+from psycopg2 import OperationalError
+import streamlit as st
 
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        database=os.getenv("DB_NAME", "signlog"),
-        user=os.getenv("DB_USER", "postgres"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    try:
+        db = st.secrets["database"]
+        conn = psycopg2.connect(
+            host=db["host"],
+            port=db["port"],
+            dbname=db["dbname"],
+            user=db["user"],
+            password=db["password"]
+        )
+        return conn
+    except OperationalError as e:
+        st.error(f"❌ Connection failed: {e}")
+        return None
