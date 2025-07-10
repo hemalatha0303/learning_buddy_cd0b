dd924d20da4692fdf1cea3e5d09d6d2c022c8157
Hemalatha Donapati <hemalathareddy.7254@gmail.com>
2025-07-10 21:52:31 +0530

    Update get_connection.py

--- get_connection.py
+++ get_connection.py
@@ -6,13 +6,13 @@ import streamlit as st
 def get_connection():
     try:
         db = st.secrets["database"]
-        conn = psycopg2.connect(
-            host="aws-0-ap-south-1.pooler.supabase.com",
-            port="6543",
-            database="postgres",
-            user="postgres",
-            password="Hema!@#7254",
-            sslmode="require"  # 🔒 this line is crucial
+        return psycopg2.connect(
+            host=db["host"],
+            port=db["port"],
+            database=db["dbname"],
+            user=db["user"],
+            password=db["password"],
+            sslmode="require"
         )
         return conn
     except OperationalError as e:
