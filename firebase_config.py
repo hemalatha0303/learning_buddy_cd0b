db3eed982d520377eb5a978a9c497e64fe0d04b6
Hemalatha Donapati <hemalathareddy.7254@gmail.com>
2025-07-16 22:15:12 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -3,7 +3,11 @@ import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
+cred_json = os.environ.get("FIREBASE_CREDENTIALS_JSON")
+
+if not cred_json:
+    raise ValueError("🔥 FIREBASE_CREDENTIALS_JSON is not set in Streamlit secrets or environment variables.")
+
 cred = credentials.Certificate(json.loads(cred_json))
 
 if not firebase_admin._apps:
