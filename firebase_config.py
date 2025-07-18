50017eaa57831e9f85b3c02fb3d9f658e1caba36
Hemalatha Donapati <hemalathareddy.7254@gmail.com>
2025-07-18 18:56:32 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -5,7 +5,7 @@ import streamlit as st
 @st.cache_resource
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("firebase_credentials.json")
+        cred = credentials.Certificate("firebase_credentials.json")  # Place this file securely
         firebase_admin.initialize_app(cred)
     return firestore.client()
 
