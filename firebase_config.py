ea19bb0b491a6839b44bace99c147994a1649c13
Hemalatha Donapati <hemalathareddy.7254@gmail.com>
2025-07-18 18:37:19 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,22 +1,22 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore
 import tempfile
 import streamlit as st
-from dotenv import load_dotenv
-
-load_dotenv()
 
 @st.cache_resource
 def init_firebase():
-    cred_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
+    firebase_creds_json = st.secrets["firebase"]["FIREBASE_CREDENTIALS_JSON"]
+
     with tempfile.NamedTemporaryFile(delete=False, suffix=".json", mode='w') as tmp:
-        tmp.write(cred_json)
+        tmp.write(firebase_creds_json)
         tmp_path = tmp.name
 
     cred = credentials.Certificate(tmp_path)
-    firebase_admin.initialize_app(cred)
+
+    # Avoid re-initializing if already done
+    if not firebase_admin._apps:
+        firebase_admin.initialize_app(cred)
+
     return firestore.client()
 
 db = init_firebase()
