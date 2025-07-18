a4f8b067508231d2a9245f1e3daecb5627ef2008
Hemalatha Donapati <hemalathareddy.7254@gmail.com>
2025-07-18 20:37:11 +0530

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,23 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-import streamlit as st
-import tempfile
 import json
-import os
+import streamlit as st
 
 @st.cache_resource
 def init_firebase():
     if not firebase_admin._apps:
-        cred_json = st.secrets["firebase"]["FIREBASE_CREDENTIALS_JSON"]
-        data = json.loads(cred_json)
-
-        with tempfile.NamedTemporaryFile(mode="w+", delete=False, suffix=".json") as f:
-            json.dump(data, f)
-            tmp_path = f.name
-
-        cred = credentials.Certificate(tmp_path)
+        json_str = st.secrets["firebase"]["FIREBASE_CREDENTIALS_JSON"]
+        cred_dict = json.loads(json_str)
+        cred = credentials.Certificate(cred_dict)
         firebase_admin.initialize_app(cred)
-
     return firestore.client()
 
 db = init_firebase()
